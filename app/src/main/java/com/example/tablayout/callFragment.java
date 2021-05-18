439e43b796dd5ac75c0387d86187412f346e4baa
Anjali Bindal <anjalibindal628@gmail.com>
2021-05-18 13:52:59 +0530

    first commit

---
+++ app/src/main/java/com/example/tablayout/callFragment.java
@@ -0,0 +1,22 @@
+package com.example.tablayout;
+
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class callFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.callsfragment,null);
+    }
+}
